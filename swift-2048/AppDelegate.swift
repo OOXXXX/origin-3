6fe3d45cab95ee7304a5cb54da9845b86ac5d37b
Patrick <zhupx2003@126.com>
2019-06-19 23:17:39 +0800

    bug fix

--- swift-2048/AppDelegate.swift
+++ swift-2048/AppDelegate.swift
@@ -2,9 +2,6 @@
 //  AppDelegate.swift
 //  swift-2048
 //
-//  Created by Austin Zheng on 6/3/14.
-//  Copyright (c) 2014 Austin Zheng. Released under the terms of the MIT license.
-//
 
 import UIKit
 
